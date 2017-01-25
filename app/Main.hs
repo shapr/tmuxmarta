fd04aef9e633eee0b53037c7fba1af307aee7aaf
Shae Erisson <shae@scannedinavian.com>
2017-01-24 20:30:25 -0500

    changes submitted by Brandon Michael Moore

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import           Data.Aeson
 import qualified Lib        as L
 
 main :: IO ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TransformListComp #-}
 module Lib ( main ) where
 
-import           Control.Arrow        ((&&&))
-import           Control.Lens
-import           Data.Aeson
-import           Data.Aeson.Types
-import qualified Data.ByteString.Lazy as BSL
-import           Data.Char            (toLower)
-import qualified Data.HashMap.Strict  as HM
-import           Data.List            (groupBy, intercalate, nub, sortOn)
-import           Data.Maybe           (fromJust)
-import qualified Data.Text            as T
-import           GHC.Generics
-import           Network.Wreq
-import           System.Directory     (getHomeDirectory)
+import           Control.Lens        (Getter, makeLenses, to, (%~), (^.))
+import           Data.Aeson.Lens     (_Object)
+import           Data.Aeson.Types    (FromJSON (parseJSON),
+                                      Options (fieldLabelModifier), Value,
+                                      defaultOptions, genericParseJSON)
+import           Data.Char           (toLower)
+import qualified Data.HashMap.Strict as HM
+import           Data.List           (intercalate)
+import qualified Data.Text           as T
+import           GHC.Exts            (groupWith, the)
+import           GHC.Generics        (Generic)
+import           Network.Wreq        (asJSON, get, responseBody)
+import           System.Directory    (getHomeDirectory)
 
 {- config will need to know the line for some people so match on line as well! feature request?
 desired output: M:2,8 N:3,7 -}
@@ -23,11 +23,6 @@ desired output: M:2,8 N:3,7 -}
 config = [("MIDTOWN STATION",N), ("NORTH AVE STATION",S), ("LINDBERGH STATION",S)]
 
 url = "http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey="
-
-jsonLower :: Value -> Value
-jsonLower (Object o) = Object . HM.fromList . map lowerPair . HM.toList $ o
-  where lowerPair (key,val) = (T.toLower key, val)
-jsonLower x = x
 
 data Direction = N | E | S | W deriving (Show, Eq, Generic)
 
@@ -39,8 +34,6 @@ data Line = BLUE | GOLD | GREEN | RED deriving (Show, Eq, Generic)
 instance FromJSON Line where
   parseJSON = genericParseJSON defaultOptions
 
-
--- data Event =
 data Event = Event {
   _waiting_time      :: String
   , _waiting_seconds :: String
@@ -54,35 +47,24 @@ data Event = Event {
   } deriving (Show, Generic, Eq)
 
 makeLenses ''Event
+waiting_minutes :: Getter Event String
+waiting_minutes = waiting_time . to (takeWhile (/=' '))
 
 instance FromJSON Event where
   parseJSON = genericParseJSON opts . jsonLower
     where opts = defaultOptions { fieldLabelModifier = map toLower . drop 1}
 
-foo = do contents <- BSL.readFile "input.txt"
-         print $ (decode contents :: Maybe [Event])
-
-dec s = do contents <- BSL.readFile s
-           return (decode contents :: Maybe [Event])
-
-matchStation :: String -> Direction -> Event -> Bool
-matchStation s d e = view station e == s && view direction e == d
-
-checks = uncurry matchStation <$> config
-my_stations es cs = [(view waiting_time e, view station e) | e <- es , c <- cs , c e]
-
-snip (x,y) = (takeWhile (/= ' ') x,take 1 y)
-type Rev = Response [Event]
+jsonLower :: Value -> Value
+jsonLower = _Object %~ mapKeys T.toLower
+  where mapKeys f hash = HM.fromList [(f k,v) | (k,v) <- HM.toList hash]
 
 main = do
   homedir <- getHomeDirectory
   apikey <- readFile (homedir ++ "/.martakey")
-  r <- asJSON =<< get (url ++ apikey) :: IO Rev
-  print $ map splot $ map (desig &&& mins) (chunkit $ my_stations (r ^. responseBody) checks)
-
-chunkit c = map (take 2) $ Data.List.groupBy (\x y -> snd y == snd x) $ sortOn snd $ snip <$> c
-
-desig as = head . nub $ map snd as
-mins as = intercalate "," $ fst <$> as
-
-splot (a,b) = a ++ ":" ++ b
+  r <- asJSON =<< get (url ++ apikey)
+  print $ [take 1 (the station)++":"++intercalate "," (take 2 minute)
+          | event <- r ^. responseBody
+          , station <- [event ^. station]
+          , (station, event ^. direction) `elem` config
+          , let minute = event ^. waiting_minutes
+          , then group by station using groupWith]
